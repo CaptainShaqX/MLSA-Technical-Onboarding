48c275d95a0fb36ea492868cb3a73a6379162585
Bhawesh Choudhary <114644299+CaptainShaqX@users.noreply.github.com>
2024-02-08 20:26:00 +0000

    home- portfolio

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/269622.jpg";
+import image from "../images/16677.jpg";
 
 const imageAltText = "Homepage background image";
 
